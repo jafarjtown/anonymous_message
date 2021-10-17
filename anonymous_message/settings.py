3f0203d07f842e72e74e95c05b4133e663b095cf
jafarjtown <57459852+jafarjtown@users.noreply.github.com>
2021-10-17 09:59:00 +0100

    Update settings.py

--- anonymous_message/settings.py
+++ anonymous_message/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
